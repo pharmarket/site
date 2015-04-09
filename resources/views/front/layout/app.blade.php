012e5dbb4ab8cdf98c4242f30f2e351b73eb787d
William Langlois <wla@net-sense.eu>
2015-04-09 15:48:43 +0200

    clean general style

--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<title>Pharmarket</title>
 	<meta name="description" content="Boutique actif grace à Laravel">
-	<meta name="generator" content="PrestaShop">
+	<meta name="generator" content="">
 	<meta name="robots" content="index,follow">
 	<meta name="viewport" content="width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0">
 	<meta name="apple-mobile-web-app-capable" content="yes">
@@ -23,29 +23,12 @@
 	<link rel="stylesheet" href="css/default-boostrap/css/responsive-tables.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/default-boostrap/css/uniform.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/default-boostrap/css/jquery.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockbanner.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/default-boostrap/css/product_list.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockbestsellers.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockcart.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/jquery_003.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockcategories.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockcurrencies.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockfacebook.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blocklanguages.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockcontact.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockmyaccount.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blocknewproducts.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blocknewsletter.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blocksearch.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/jquery_002.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockspecials.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blocktags.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockuserinfo.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blockviewed.css" type="text/css" media="all">
+
 	<link rel="stylesheet" href="css/default-boostrap/css/homeslider.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/default-boostrap/css/homefeatured.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/default-boostrap/css/hooks.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/default-boostrap/css/blocktopmenu.css" type="text/css" media="all">
+
 	<link rel="stylesheet" href="css/default-boostrap/css/superfish-modified.css" type="text/css" media="all">
 	<script src="css/default-boostrap/css/all.js" id="facebook-jssdk"></script><script type="text/javascript">
 	var CUSTOMIZE_TEXTFIELD = 1;
@@ -54,8 +37,8 @@
 	var FancyboxI18nPrev = 'Pr&eacute;c&eacute;dent';
 	var ajax_allowed = true;
 	var ajaxsearch = true;
-	var baseDir = 'http://localhost:8888/prestashop/';
-	var baseUri = 'http://localhost:8888/prestashop/';
+	var baseDir = '/';
+	var baseUri = '/';
 	var blocksearch_type = 'top';
 	var contentOnly = false;
 	var customizationIdMessage = 'Personnalisation';
@@ -70,7 +53,7 @@
 	var homeslider_speed = 500;
 	var homeslider_width = 779;
 	var id_lang = 1;
-	var img_dir = 'http://localhost:8888/prestashop/css/default-bootstrap/img/';
+	var img_dir = '/css/default-bootstrap/img/';
 	var instantsearch = false;
 	var isGuest = 0;
 	var isLogged = 0;
@@ -82,7 +65,7 @@
 	var quickView = true;
 	var removingLinkText = 'supprimer cet article du panier';
 	var roundMode = 2;
-	var search_url = 'http://localhost:8888/prestashop/recherche';
+	var search_url = '/recherche';
 	var static_token = 'c205de65ddcd1e67a4aad110be2047bc';
 	var toBeDetermined = 'À définir';
 	var token = 'd94dd8b4fd08fce79e988f5d5460c4cc';
@@ -116,19 +99,19 @@
 					<div class="col-sm-6">
 						<div class="contactinfo">
 							<ul class="nav nav-pills">
-								<li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
 							</ul>
 						</div>
 					</div>
 					<div class="col-sm-6">
 						<div class="social-icons pull-right">
 							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-google-plus"></i></a></li>
 							</ul>
 						</div>
 					</div>
@@ -141,7 +124,7 @@
 				<div class="row">
 					<div class="col-sm-4">
 						<div class="logo pull-left">
-							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+							<a href="{{URL::to('/')}}/index.html"><img src="images/home/logo.png" alt="" /></a>
 						</div>
 						<div class="btn-group pull-right">
 							<div class="btn-group">
@@ -150,8 +133,8 @@
 									<span class="caret"></span>
 								</button>
 								<ul class="dropdown-menu">
-									<li><a href="#">Canada</a></li>
-									<li><a href="#">UK</a></li>
+									<li><a href="{{URL::to('/')}}/#">Canada</a></li>
+									<li><a href="{{URL::to('/')}}/#">UK</a></li>
 								</ul>
 							</div>
 
@@ -161,8 +144,8 @@
 									<span class="caret"></span>
 								</button>
 								<ul class="dropdown-menu">
-									<li><a href="#">Canadian Dollar</a></li>
-									<li><a href="#">Pound</a></li>
+									<li><a href="{{URL::to('/')}}/#">Canadian Dollar</a></li>
+									<li><a href="{{URL::to('/')}}/#">Pound</a></li>
 								</ul>
 							</div>
 						</div>
@@ -170,11 +153,11 @@
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
-								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
-								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-user"></i> Account</a></li>
+								<li><a href="{{URL::to('/')}}/#"><i class="fa fa-star"></i> Wishlist</a></li>
+								<li><a href="{{URL::to('/')}}/checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="{{URL::to('/')}}/cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+								<li><a href="{{URL::to('/')}}/login.html"><i class="fa fa-lock"></i> Login</a></li>
 							</ul>
 						</div>
 					</div>
@@ -196,24 +179,24 @@
 						</div>
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="index.html" class="active">Home</a></li>
-								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+								<li><a href="{{URL::to('/')}}/index.html" class="active">Home</a></li>
+								<li class="dropdown"><a href="{{URL::to('/')}}/#">Shop<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="shop.html">Products</a></li>
-										<li><a href="product-details.html">Product Details</a></li>
-										<li><a href="checkout.html">Checkout</a></li>
-										<li><a href="cart.html">Cart</a></li>
-										<li><a href="login.html">Login</a></li>
+                                        <li><a href="{{URL::to('/')}}/shop.html">Products</a></li>
+										<li><a href="{{URL::to('/')}}/product-details.html">Product Details</a></li>
+										<li><a href="{{URL::to('/')}}/checkout.html">Checkout</a></li>
+										<li><a href="{{URL::to('/')}}/cart.html">Cart</a></li>
+										<li><a href="{{URL::to('/')}}/login.html">Login</a></li>
                                     </ul>
                                 </li>
-								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
+								<li class="dropdown"><a href="{{URL::to('/')}}/#">Blog<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
+                                        <li><a href="{{URL::to('/')}}/blog.html">Blog List</a></li>
+										<li><a href="{{URL::to('/')}}/blog-single.html">Blog Single</a></li>
                                     </ul>
                                 </li>
-								<li><a href="404.html">404</a></li>
-								<li><a href="contact-us.html">Contact</a></li>
+								<li><a href="{{URL::to('/')}}/404.html">404</a></li>
+								<li><a href="{{URL::to('/')}}/contact-us.html">Contact</a></li>
 							</ul>
 						</div>
 					</div>
@@ -239,7 +222,7 @@
 				<div id="newsletter_block_left" class="block">
 				<h4>Lettre d'informations</h4>
 				<div class="block_content">
-				<form action="//localhost:8888/prestashop/" method="post">
+				<form action="/" method="post">
 				<div class="form-group">
 				<input class="inputNew form-control grey newsletter-input" id="newsletter-input" name="email" size="18" value="Saisissez votre adresse e-mail" type="text">
 								<button type="submit" name="submitNewsletter" class="btn btn-default button button-small">
@@ -254,22 +237,22 @@
 				<section id="social_block" class="pull-right">
 				<ul>
 					<li class="facebook">
-				<a target="_blank" class="_blank" href="http://www.facebook.com/prestashop">
+				<a target="_blank" class="_blank" href="http://www.facebook.com/">
 					<span>Facebook</span>
 				</a>
 				</li>
 							<li class="twitter">
-				<a target="_blank" class="_blank" href="http://www.twitter.com/prestashop">
+				<a target="_blank" class="_blank" href="http://www.twitter.com/">
 					<span>Twitter</span>
 				</a>
 				</li>
 							<li class="rss">
-				<a target="_blank" class="_blank" href="http://www.prestashop.com/blog/en/">
+				<a target="_blank" class="_blank" href="http://www..com/blog/en/">
 					<span>RSS</span>
 				</a>
 				</li>
 															<li class="google-plus">
-						<a target="_blank" class="_blank" href="https://www.google.com/+prestashop">
+						<a target="_blank" class="_blank" href="https://www.google.com/+">
 							<span>Google Plus</span>
 						</a>
 					</li>
@@ -285,7 +268,7 @@
 				<ul style="display: block;" class="tree dynamized">
 
 				<li class="last">
-				<span class="grower CLOSE"> </span><a href="http://localhost:8888/prestashop/3-femmes" title="Vous trouverez ici toutes les collections mode pour femmes.
+				<span class="grower CLOSE"> </span><a href="{{URL::to('/')}}//3-femmes" title="Vous trouverez ici toutes les collections mode pour femmes.
 				Cette catégorie regroupe tous les basiques de votre garde-robe et bien plus encore&nbsp;:
 				chaussures, accessoires, T-shirts imprimés, robes élégantes et jeans pour femmes&nbsp;!">
 				Femmes
@@ -293,14 +276,14 @@
 				<ul style="display: none;">
 
 				<li>
-				<span class="grower CLOSE"> </span><a href="http://localhost:8888/prestashop/4-tops" title="Choisissez parmi une large sélection de T-shirts à manches courtes, longues ou 3/4, de tops, de débardeurs, de chemisiers et bien plus encore.
+				<span class="grower CLOSE"> </span><a href="{{URL::to('/')}}//4-tops" title="Choisissez parmi une large sélection de T-shirts à manches courtes, longues ou 3/4, de tops, de débardeurs, de chemisiers et bien plus encore.
 				Trouvez la coupe qui vous va le mieux&nbsp;!">
 				Tops
 				</a>
 				<ul style="display: none;">
 
 				<li>
-				<a href="http://localhost:8888/prestashop/5-t-shirts" title="Les must have de votre garde-robe&nbsp;: découvrez les divers modèles ainsi que les différentes
+				<a href="{{URL::to('/')}}//5-t-shirts" title="Les must have de votre garde-robe&nbsp;: découvrez les divers modèles ainsi que les différentes
 				coupes et couleurs de notre collection&nbsp;!">
 				T-shirts
 				</a>
@@ -308,7 +291,7 @@
 
 
 				<li class="last">
-				<a href="http://localhost:8888/prestashop/7-chemisiers" title="Coordonnez vos accessoires à vos chemisiers préférés, pour un look parfait.">
+				<a href="{{URL::to('/')}}//7-chemisiers" title="Coordonnez vos accessoires à vos chemisiers préférés, pour un look parfait.">
 				Chemisiers
 				</a>
 				</li>
@@ -318,14 +301,14 @@
 
 
 				<li class="last">
-				<span class="grower CLOSE"> </span><a href="http://localhost:8888/prestashop/8-robes" title="Trouvez votre nouvelle pièce préférée parmi une large sélection de robes décontractées, d'été et de soirée&nbsp;!
+				<span class="grower CLOSE"> </span><a href="{{URL::to('/')}}//8-robes" title="Trouvez votre nouvelle pièce préférée parmi une large sélection de robes décontractées, d'été et de soirée&nbsp;!
 				Nous avons des robes pour tous les styles et toutes les occasions.">
 				Robes
 				</a>
 				<ul style="display: none;">
 
 				<li>
-				<a href="http://localhost:8888/prestashop/9-robes-decontractees" title="Vous cherchez une robe pour la vie de tous les jours&nbsp;? Découvrez
+				<a href="{{URL::to('/')}}//9-robes-decontractees" title="Vous cherchez une robe pour la vie de tous les jours&nbsp;? Découvrez
 				notre sélection de robes et trouvez celle qui vous convient.">
 				Robes décontractées
 				</a>
@@ -333,14 +316,14 @@
 
 
 				<li>
-				<a href="http://localhost:8888/prestashop/10-robes-soiree" title="Trouvez la robe parfaite pour une soirée inoubliable&nbsp;!">
+				<a href="{{URL::to('/')}}//10-robes-soiree" title="Trouvez la robe parfaite pour une soirée inoubliable&nbsp;!">
 				Robes de soirée
 				</a>
 				</li>
 
 
 				<li class="last">
-				<a href="http://localhost:8888/prestashop/11-robes-ete" title="Courte, longue, en soie ou imprimée, trouvez votre robe d'été idéale&nbsp;!">
+				<a href="{{URL::to('/')}}//11-robes-ete" title="Courte, longue, en soie ou imprimée, trouvez votre robe d'été idéale&nbsp;!">
 				Robes d'été
 				</a>
 				</li>
@@ -363,42 +346,42 @@
 				<h4>Informations</h4>
 				<ul style="" class="toggle-footer">
 							<li class="item">
-					<a href="http://localhost:8888/prestashop/promotions" title="Promotions">
+					<a href="{{URL::to('/')}}//promotions" title="Promotions">
 						Promotions
 					</a>
 				</li>
 									<li class="item">
-				<a href="http://localhost:8888/prestashop/nouveaux-produits" title="Nouveaux produits">
+				<a href="{{URL::to('/')}}//nouveaux-produits" title="Nouveaux produits">
 					Nouveaux produits
 				</a>
 				</li>
 										<li class="item">
-					<a href="http://localhost:8888/prestashop/meilleures-ventes" title="Meilleures ventes">
+					<a href="{{URL::to('/')}}//meilleures-ventes" title="Meilleures ventes">
 						Meilleures ventes
 					</a>
 				</li>
 										<li class="item">
-					<a href="http://localhost:8888/prestashop/magasins" title="Nos magasins">
+					<a href="{{URL::to('/')}}//magasins" title="Nos magasins">
 						Nos magasins
 					</a>
 				</li>
 									<li class="item">
-				<a href="http://localhost:8888/prestashop/nous-contacter" title="Contactez-nous">
+				<a href="{{URL::to('/')}}//nous-contacter" title="Contactez-nous">
 					Contactez-nous
 				</a>
 				</li>
 															<li class="item">
-						<a href="http://localhost:8888/prestashop/content/3-conditions-utilisation" title="Conditions d'utilisation">
+						<a href="{{URL::to('/')}}//content/3-conditions-utilisation" title="Conditions d'utilisation">
 							Conditions d'utilisation
 						</a>
 					</li>
 																<li class="item">
-						<a href="http://localhost:8888/prestashop/content/4-a-propos" title="A propos">
+						<a href="{{URL::to('/')}}//content/4-a-propos" title="A propos">
 							A propos
 						</a>
 					</li>
 													<li>
-				<a href="http://localhost:8888/prestashop/plan-site" title="sitemap">
+				<a href="{{URL::to('/')}}//plan-site" title="sitemap">
 					sitemap
 				</a>
 				</li>
@@ -407,21 +390,21 @@
 				</section>
 				<section class="bottom-footer col-xs-12">
 				<div>
-				© 2014 <a target="_blank" class="_blank" href="http://www.prestashop.com/">Logiciel e-commerce par PrestaShop™</a>
+				© 2014 <a target="_blank" class="_blank" href="http://www..com/">Logiciel e-commerce par ™</a>
 				</div>
 				</section>
 				<!-- /MODULE Block footer -->
 				<!-- Block myaccount module -->
 				<section class="footer-block col-xs-12 col-sm-4">
-				<h4><a href="http://localhost:8888/prestashop/mon-compte" title="Gérer mon compte client" rel="nofollow">Mon compte</a></h4>
+				<h4><a href="{{URL::to('/')}}//mon-compte" title="Gérer mon compte client" rel="nofollow">Mon compte</a></h4>
 				<div style="" class="block_content toggle-footer">
 				<ul class="bullet">
-				<li><a href="http://localhost:8888/prestashop/historique-commandes" title="Mes commandes" rel="nofollow">Mes commandes</a></li>
-						<li><a href="http://localhost:8888/prestashop/avoirs" title="Mes avoirs" rel="nofollow">Mes avoirs</a></li>
-				<li><a href="http://localhost:8888/prestashop/adresses" title="Mes adresses" rel="nofollow">Mes adresses</a></li>
-				<li><a href="http://localhost:8888/prestashop/identite" title="Gérer mes informations personnelles" rel="nofollow">Mes informations personnelles</a></li>
+				<li><a href="{{URL::to('/')}}//historique-commandes" title="Mes commandes" rel="nofollow">Mes commandes</a></li>
+						<li><a href="{{URL::to('/')}}//avoirs" title="Mes avoirs" rel="nofollow">Mes avoirs</a></li>
+				<li><a href="{{URL::to('/')}}//adresses" title="Mes adresses" rel="nofollow">Mes adresses</a></li>
+				<li><a href="{{URL::to('/')}}//identite" title="Gérer mes informations personnelles" rel="nofollow">Mes informations personnelles</a></li>
 
-						<li><a href="http://localhost:8888/prestashop/?mylogout" title="Déconnexion" rel="nofollow">Déconnexion</a></li>		</ul>
+						<li><a href="{{URL::to('/')}}//?mylogout" title="Déconnexion" rel="nofollow">Déconnexion</a></li>		</ul>
 				</div>
 				</section>
 				<!-- /Block myaccount module -->
@@ -440,7 +423,7 @@
 							</li>
 																			<li>
 								<i class="icon-envelope-alt"></i>E-mail :
-								<span><a href="mailto:%73%61%6c%65%73@%79%6f%75%72%63%6f%6d%70%61%6e%79.%63%6f%6d">sales@yourcompany.com</a></span>
+								<span><a href="{{URL::to('/')}}/mailto:%73%61%6c%65%73@%79%6f%75%72%63%6f%6d%70%61%6e%79.%63%6f%6d">sales@yourcompany.com</a></span>
 							</li>
 										</ul>
 				</div>
--- resources/views/front/layout/menu.blade.php
+++ resources/views/front/layout/menu.blade.php
@@ -1,38 +1,27 @@
 
 <!-- Menu -->
-<div id="block_top_menu" class="sf-contener clearfix col-lg-12">
-  <div class="cat-title">Catégories</div>
-  <ul class="sf-menu clearfix menu-content sf-js-enabled sf-arrows">
-    <li>
-      <a class="sf-with-ul" href="" title="Femmes">Femmes</a>
-      <ul class="submenu-container clearfix first-in-line-xs" style="display: none;">
-        <li><a class="sf-with-ul" href="http://localhost:8888/prestashop/4-tops" title="Tops">Tops</a>
-          <ul style="display: none;"><li><a href="http://localhost:8888/prestashop/5-t-shirts" title="T-shirts">T-shirts</a>
-          </li>
-          <li><a href="http://localhost:8888/prestashop/7-chemisiers" title="Chemisiers">Chemisiers</a></li>
-        </ul></li>
-        <li><a class="sf-with-ul" href="http://localhost:8888/prestashop/8-robes" title="Robes">Robes</a>
-          <ul style="display: none;">
-            <li><a href="http://localhost:8888/prestashop/9-robes-decontractees" title="Robes décontractées">Robes décontractées</a>
-            </li>
-            <li><a href="http://localhost:8888/prestashop/10-robes-soiree" title="Robes de soirée">Robes de soirée</a>
-            </li>
-            <li><a href="http://localhost:8888/prestashop/11-robes-ete" title="Robes d'été">Robes d'été</a></li>
-          </ul></li><li class="category-thumbnail">
-            <div><img src="payment%20-%20mirage_fichiers/3-0_thumb.jpg" alt="Femmes" title="Femmes" class="imgm"></div>
-            <div><img src="payment%20-%20mirage_fichiers/3-1_thumb.jpg" alt="Femmes" title="Femmes" class="imgm"></div>
-          </li></ul>
-        </li>
-        <li><a class="sf-with-ul" href="http://localhost:8888/prestashop/8-robes" title="Robes">Robes</a>
-          <ul class="submenu-container clearfix first-in-line-xs" style="display: none;">
-            <li><a href="http://localhost:8888/prestashop/9-robes-decontractees" title="Robes décontractées">Robes décontractées</a></li>
-            <li><a href="http://localhost:8888/prestashop/10-robes-soiree" title="Robes de soirée">Robes de soirée</a></li>
-            <li><a href="http://localhost:8888/prestashop/11-robes-ete" title="Robes d'été">Robes d'été</a></li>
-            <li class="category-thumbnail">
-            </li>
-          </ul>
-        </li>
-        <li><a href="http://localhost:8888/prestashop/5-t-shirts" title="T-shirts">T-shirts</a></li>
-      </ul>
-    </div>
-    <!--/ Menu -->
+<div class="mainmenu pull-left">
+  <ul class="nav navbar-nav collapse navbar-collapse">
+    <li><a href="{{URL::to('/')}}/index.html" class="active">Home</a></li>
+    <li class="dropdown"><a href="{{URL::to('/')}}/#">Shop<i class="fa fa-angle-down"></i></a>
+                        <ul role="menu" class="sub-menu">
+                            <li><a href="{{URL::to('/')}}/shop.html">Products</a></li>
+        <li><a href="{{URL::to('/')}}/product-details.html">Product Details</a></li>
+        <li><a href="{{URL::to('/')}}/checkout.html">Checkout</a></li>
+        <li><a href="{{URL::to('/')}}/cart.html">Cart</a></li>
+        <li><a href="{{URL::to('/')}}/login.html">Login</a></li>
+                        </ul>
+                    </li>
+    <li class="dropdown"><a href="{{URL::to('/')}}/#">Blog<i class="fa fa-angle-down"></i></a>
+                        <ul role="menu" class="sub-menu">
+                            <li><a href="{{URL::to('/')}}/blog.html">Blog List</a></li>
+        <li><a href="{{URL::to('/')}}/blog-single.html">Blog Single</a></li>
+                        </ul>
+                    </li>
+    <li><a href="{{URL::to('/')}}/404.html">404</a></li>
+    <li><a href="{{URL::to('/')}}/contact-us.html">Contact</a></li>
+  </ul>
+</div>
+
+
+<!--/ Menu -->
