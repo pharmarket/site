ffc2a8b97a8c9c428b9c9897ff0fdfdb25e8ae76
dimitry-gosselin <dimitry.gosselin@gmail.com>
2015-05-07 18:47:30 +0200

    Ajout d'une page 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,6 +36,7 @@ class Handler extends ExceptionHandler {
 	 */
 	public function render($request, Exception $e)
 	{
+		if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException){return response()->view('front.errors.404', [], 404); }
 		return parent::render($request, $e);
 	}
 
---
+++ resources/views/front/errors/404.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('front/images/chuck-norris.jpg')}}" width="100%" height="100%" />
