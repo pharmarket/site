8b7a0b82ee8f43d19d1a3b119ae8ab5c457cf309
dimitry-gosselin <dimitry.gosselin@gmail.com>
2015-06-02 02:45:22 +0200

    Refonte des requetes

--- app/Produit.php
+++ app/Produit.php
@@ -27,28 +27,19 @@ class Produit extends Model {
 	public static function lastProducts()	{
 
 		$idlang = Lang::where('code' , 'like' , App::getLocale())->first() ;
-		$last = array();
 
+		$produit = DB::table('produit_info')	->join('produit', 'produit.id', '=', 'produit_info.produit_id')
+							->join('media', 'produit.id', '=', 'media.produit_id')
+							->where('produit_info.langue_id', '=', $idlang->id)
+							->where('media.langue_id', '=', $idlang->id)
+						            ->select('produit_info.nom', 'produit_info.description', 'produit.id', 'chemin')
+						            ->orderBy('produit.created_at','DESC')
+						            ->groupBy('produit.id')
+						            ->where('default', 1)
+						            ->take(10)
+						            ->get();
 
-		foreach (Produit::orderBy('created_at','DESC')->take(10)->get() as $key => $row) {
-
-			if (Media::where('produit_id' , '=' , $row->attributes['id'])) {
-				$nom = DB::table('produit_info')
-													->where('produit_id', '=', $row->attributes['id'])
-													->where('langue_id', '=', $idlang->attributes['id'])
-							            ->select('nom', 'description')
-							            ->first();
-				$last[$key] = array();
-				$last[$key]['id'] = $row->attributes['id'];
-				$last[$key]['nom'] = (isset($nom->nom)?$nom->nom:"undefined");
-				$last[$key]['description'] = (isset($nom->description)?$nom->description:"undefined");
-				$media = Media::where('produit_id' , '=' , $row->attributes['id'])->first();
-				$last[$key]['media'] = $media->attributes['chemin'].$media->attributes['nom'] ;
-			}
-
-		}
-
-		return $last;
+		return $produit;
 	}
 
 	/**
@@ -57,45 +48,20 @@ class Produit extends Model {
 	* @return Array of Product
 	*/
 	public static function bestProducts()	{
-
-	/*	SELECT id, count( `exemplaire_id` )
-		FROM `commande_exemplaire`
-		WHERE 1
-		GROUP BY id
-		LIMIT 0 , 30
-		*/
 		$idlang = Lang::where('code' , 'like' , App::getLocale())->first() ;
-		$bestProducts = array();
+		$produit = DB::table('produit_info')	->join('produit', 'produit.id', '=', 'produit_info.produit_id')
+							->join('media', 'produit.id', '=', 'media.produit_id')
+							->join('produit_exemplaire', 'produit.id', '=', 'produit_exemplaire.produit_id')
+							->join('commande_exemplaire', 'commande_exemplaire.exemplaire_id', '=', 'produit_exemplaire.id')
+							->where('produit_info.langue_id', '=', $idlang->id)
+							->where('media.langue_id', '=', $idlang->id)
+						            ->select('produit_info.nom', 'produit_info.description', 'produit.id', 'chemin', DB::raw('count( `exemplaire_id` ) as count'))
+						            ->orderBy('count','DESC')
+						            ->groupBy('produit.id')
+						            ->where('default', 1)
+						            ->take(10)
+						            ->get();
 
-		$best = DB::table('commande_exemplaire')
-		                     ->select(DB::raw('id,count( `exemplaire_id` ) as count'))
-		                     ->groupBy('id')
-												 ->orderBy('count','DESC')
-		                     ->get();
-		$listbest = array( );
-
-		foreach ($best as $key => $row) {
-			$listbest[$key]= $row->id;
-		}
-
-		foreach (Produit::orderBy('created_at','DESC') ->whereIn('id',$listbest)->take(10)->get() as $key => $row) {
-
-			if (Media::where('produit_id' , '=' , $row->attributes['id'])) {
-				$nom = DB::table('produit_info')
-													->where('produit_id', '=', $row->attributes['id'])
-													->where('langue_id', '=', $idlang->attributes['id'])
-							            ->select('nom', 'description')
-							            ->first();
-				$labestProductsst[$key] = array();
-				$bestProducts[$key]['id'] = $row->attributes['id'];
-				$bestProducts[$key]['nom'] = (isset($nom->nom)?$nom->nom:"undefined");
-				$bestProducts[$key]['description'] = (isset($nom->description)?$nom->description:"undefined");
-				$media = Media::where('produit_id' , '=' , $row->attributes['id'])->first();
-				$bestProducts[$key]['media'] = $media->attributes['chemin'].$media->attributes['nom'] ;
-			}
-
-		}
-
-		return $bestProducts;
+		return $produit;
 	}
 }
