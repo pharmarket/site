0bd9f7bc2d71f2fd314f33f319500916d507b99a
lemirage <william91120@hotmail.com>
2015-06-09 12:17:20 +0200

    Google analytick

--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -22,6 +22,16 @@
 
    <link rel="stylesheet" type="text/css" href="{{ asset('front/css/switcher.css') }}" media="screen" />
     <link rel="stylesheet" type="text/css" href="{{ asset('front/css/menu.css') }}" media="screen" />
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63542056-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
     @yield('header')
    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
